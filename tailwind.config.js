3d1ee613fa64aa6aa6d8d2ac11ac88383c65a7cc
drone-cr2 <pradyumnamaydeo@gmail.com>
2024-04-27 21:14:59 +0530

    removed extra colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,11 @@ export default {
   ], theme: {
     extend: {
       colors: {
-        "moss-green": "#606c38ff",
-        "dark-green": "#283618ff",
         "cornsilk": "#fefae0ff",
         "earth-yellow": "#dda15eff",
         "tiger-yellow": "#bc6c25ff",
       },
       textColor :{
-        "moss-green": "#606c38ff",
-        "dark-green": "#283618ff",
         "cornsilk": "#fefae0ff",
         "earth-yellow": "#dda15eff",
         "tiger-yellow": "#bc6c25ff",
